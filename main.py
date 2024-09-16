a3e709559c708b86ecec58862e0a42ef7532e043
AltanaZhambaldorzhieva <zhambaldorzhieva2005@mail.ru>
2024-09-16 19:32:27 +0700

    2_com

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Case_6
+# Developers: Zhambaldorzhieva A., Makarenko K.
+#
+import ru_local as ru
+
 text = input().strip()
 sent = -text.count('...')*2 + text.count('?') + text.count('!') + text.count('.')
 
