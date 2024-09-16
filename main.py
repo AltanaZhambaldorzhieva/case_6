2327f19fc77bc2744b5c7aab671c628a4bafa4a8
maKARINka <k.makaremko@g.nsu.ru>
2024-09-16 19:38:30 +0700

    2

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-text = input().strip()
-sent = -text.count('...')*2 + text.count('?') + text.count('!') + text.count('.')
+def snt(txt):
+    cnt = txt.count('.') + txt.count('?') + txt.count('!')
+    cnt += - txt.count('...')*2 - txt.count('???')*2 - txt.count('!!!')*2
+    return cnt
+def wrd(txt):
+    a = len(txt.split())
+    return a
 
+txt = input().strip()
+print(snt(txt))
+print(wrd(txt))
