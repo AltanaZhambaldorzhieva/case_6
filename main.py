170d592affedb8465f2e22856424d3062d5e58ee
AltanaZhambaldorzhieva <zhambaldorzhieva2005@mail.ru>
2024-09-18 00:21:25 +0700

    10

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 # Developers: Zhambaldorzhieva A., Makarenko K.
 #
 import ru_local as ru
-import goslate
+from translate import Translator
 from textblob import TextBlob
-
 
 
 def sent(txt):
@@ -12,7 +11,7 @@ def sent(txt):
         The function returns the number of sentences in the text.
     """
     count = txt.count('.') + txt.count('?') + txt.count('!')
-    count += - txt.count('...')*2 - txt.count('???')*2 - txt.count('!!!')*2
+    count += - txt.count('...') * 2 - txt.count('???') * 2 - txt.count('!!!') * 2
     return count
 
 
@@ -42,7 +41,7 @@ def avg_sent(wrd, sent):
     """
         The function returns the average sentence length in words.
     """
-    avg = wrd/sent
+    avg = wrd / sent
     return avg
 
 
@@ -50,7 +49,7 @@ def avg_wrd(wrd, syl):
     """
         The function returns the average word length in syl.
     """
-    avg = syl/wrd
+    avg = syl / wrd
     return avg
 
 
@@ -76,42 +75,42 @@ def ton(txt):
     """
     pol = TextBlob(txt).sentiment.polarity
     if pol > 0:
-        return(f'{ru.TON_POS}')
+        return (f'{ru.TON_POS}')
     elif pol < 0:
-        return(f'{ru.TON_NEG}')
+        return (f'{ru.TON_NEG}')
     else:
-        return(f'{ru.TON_NORM}')
+        return (f'{ru.TON_NORM}')
 
 
 def subjectivity(txt):
     """
         The function returns the subjectivity of the text.
     """
-    sub = TextBlob(txt).subjectivity 
-    return sub*100
+    sub = TextBlob(txt).subjectivity
+    return sub * 100
+
 
 def detect_lang(txt):
     gs = goslate.Goslate()
     language_id = gs.detect(txt)
     return language_id
 
-def translate_en(txt):
-    gs = goslate.Goslate()
-    translated = gs.translate(txt, 'en')
-    return translated
 
-
+def translate(txt):
+    translator = Translator(from_lang="ru", to_lang="en")
+    translation = translator.translate(txt)
+    return translation
 
 
 count = 0
 print(f'{ru.TXT}:')
 txt = input().strip()
-print(detect_lang(txt))
+print(translate(txt))
 print(f'{ru.SENT}:', sent(txt))
 print(f'{ru.WRD}:', wrd(txt))
 print(f'{ru.SYL}:', syl(txt))
 print(f'{ru.SENT_AVR_LEN}:', avg_sent(wrd(txt), sent(txt)))
 print(f'{ru.WRD_AVR_LEN}:', avg_wrd(wrd(txt), syl(txt)))
-print(f'{ru.IND_FLASH}:', index_Flesh_eng(avg_sent(wrd(txt), sent(txt)),avg_wrd(wrd(txt), syl(txt))))
+print(f'{ru.IND_FLASH}:', index_Flesh_eng(avg_sent(wrd(txt), sent(txt)), avg_wrd(wrd(txt), syl(txt))))
 print(f'{ru.TON}:', ton(txt))
 print(f'{ru.OBJCT}:', subjectivity(txt), '%')
